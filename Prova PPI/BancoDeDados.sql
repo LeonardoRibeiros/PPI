47b84284fa47a386bbfe70e03cbe267d629323f5
Leonardo Ribeiro <111136680+LeonardoRibeiros@users.noreply.github.com>
2023-06-20 21:31:23 -0300

    Update BancoDeDados.sql

--- Prova PPI/BancoDeDados.sql
+++ Prova PPI/BancoDeDados.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE IF NOT EXISTS ppidatabase;
 USE ppidatabase;
 
-CREATE TABLE `Animal` (
-    `ID` int  NOT NULL auto_increment,
+CREATE TABLE `animal` (
+    `id` int  NOT NULL auto_increment,
     `nome` varchar(100)  NOT NULL ,
     `raca` varchar(50)  NOT NULL ,
     `idade` int  NOT NULL ,
     `peso` float  NOT NULL ,
-    PRIMARY KEY (`ID`)
+    PRIMARY KEY (`id`)
 );
 
