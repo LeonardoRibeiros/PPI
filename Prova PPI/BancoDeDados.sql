8ba27018b1add71f17dc53ed0ced87a0d539da65
Leonardo Ribeiro <111136680+LeonardoRibeiros@users.noreply.github.com>
2023-06-20 22:05:19 -0300

    Update BancoDeDados.sql

--- Prova PPI/BancoDeDados.sql
+++ Prova PPI/BancoDeDados.sql
@@ -3,6 +3,8 @@ USE ppidatabase;
 
 CREATE TABLE `animal` (
     `id` int  NOT NULL auto_increment,
+    `email` varchar(50)  NOT NULL ,
+    `senha` varchar(50)  NOT NULL ,
     `nome` varchar(100)  NOT NULL ,
     `raca` varchar(50)  NOT NULL ,
     `idade` int  NOT NULL ,
